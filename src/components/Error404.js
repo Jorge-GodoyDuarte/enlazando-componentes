1031834410f35e9f69c826e226c56f69ea3f15ba
Jorge-GodoyDuarte <hawkaa666@gmail.com>
2023-01-15 22:49:38 +0100

    TP FINISHED!!!

---
+++ src/components/Error404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Error404() {
+    return (
+        <div className="container-fluid">
+    
+                <h1 className="h3 mb-0 text-gray-800">Error 404 !!</h1>
+                <p>Seleccione otra ruta porfas!</p>
+                <img src="https://http.cat/404" alt="gatito" />
+
+        </div>
+    )
+}
